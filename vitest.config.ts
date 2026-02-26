3ceb238e5a58325a1af8358a677e8aaed15be085
vic3r <victor.g64r@gmail.com>
2026-02-26 16:51:25 -0600

    add vite config test threshold

--- vitest.config.ts
+++ vitest.config.ts
@@ -21,6 +21,12 @@ export default defineConfig({
                 'src/dto/**',
                 'src/models/**',
             ],
+            thresholds: {
+                statements: 80,
+                branches: 80,
+                functions: 80,
+                lines: 80,
+            },
         },
     },
     resolve: {
